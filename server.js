db67bfcb627692a8712b646ee5f148ed080887e4
B.J. Rossiter <b.j.rossiter@gmail.com>
2013-02-10 21:45:39 +1100

    Add compression to static server

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ var express = require('express');
 var app = express();
 
 app.configure(function() {
+    app.use(express.compress());
     app.use(express.static(__dirname + '/dist'));
     app.use(express.errorHandler({
         dumpException: true,
