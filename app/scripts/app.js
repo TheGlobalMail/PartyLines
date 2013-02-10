a8fb0b3229fb771bbc9ca3975b3819325836855b
Andrew Cobby <andrew.cobby@gmail.com>
2013-02-11 09:33:12 +1100

    Push date legend behind slider chart

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -191,7 +191,7 @@
       })
       .attr("height", fullHeight);
 
-    var dateLegendContainer = d3.select('#chart-container svg').append('g').attr('class', 'date-legend-container');
+    var dateLegendContainer = d3.select('#chart-container svg').insert('g', ':first-child').attr('class', 'date-legend-container');
     var dateLegendBackground = dateLegendContainer.append('rect');
 
     var dateLegendText = dateLegendContainer.append('text')
