81ef0c3e016a63a6ef26c69ab6009810f2d4499c
Andrew Cobby <andrew.cobby@gmail.com>
2013-02-11 14:51:56 +1100

    Reverted share URL updater, this should happen automagically

--- app/scripts/social-buttons.js
+++
@@ -1,9 +0,0 @@
-(function(app, $, location) {
-  'use strict';
-
-  var $tweetButton = $('.twitter-share-button');
-
-  app.vent.on('route', function() {
-    $tweetButton.attr('data-url', location.toString());
-  });
-}(app, $, window.location));
