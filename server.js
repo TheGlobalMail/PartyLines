85097b441d18ebe7e0212a13eabfc03f05dd1b09
Andrew Cobby <andrew.cobby@gmail.com>
2013-02-11 15:58:43 +1100

    pushState friendly server

--- server.js
+++ server.js
@@ -1,18 +1,19 @@
-var express = require('express');
-var app = express();
+// Require libraries
+var fs = require("fs");
+var express = require("express");
+var site = express();
 
-app.configure(function() {
-    app.use(express.compress());
-    app.use(express.static(__dirname + '/dist'));
-    app.use(express.errorHandler({
-        dumpException: true,
-        showStask: true
-    }))
+// Var up, bro
+var prefix = "server ~ ";
+
+// Serve static files
+site.use("/", express.static(__dirname + '/dist'));
+
+// Ensure all routes go home, client side app..
+site.get("*", function(req, res) {
+  fs.createReadStream(__dirname + "/dist/index.html").pipe(res);
 });
 
-app.get('/', function(req, res) {
-    res.redirect("/index.html");
-});
-
-app.listen(process.env.PORT || 8080);
-console.log('Started on', process.env.PORT || 8080);
+// Actually listen
+site.listen(process.env.PORT || 8080);
+console.log('Server listening on ' + process.env.PORT || 8080);
