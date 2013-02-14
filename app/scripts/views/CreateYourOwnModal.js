531715646c73028a5c3599aebd6e8a7c619ea06d
Andrew Cobby <andrew.cobby@gmail.com>
2013-02-15 10:49:09 +1100

    Start capturing form submit

--- app/scripts/views/CreateYourOwnModal.js
+++ app/scripts/views/CreateYourOwnModal.js
@@ -7,7 +7,13 @@
 
   var CreateYourOwnModal = Backbone.View.extend({
 
+    events: {
+      'submit form': 'onSubmit'
+    },
+
     initialize: function() {
+      _.bindAll(this, 'onSubmit');
+      this.$form = this.$('form');
       this.listenTo(app.vent, 'search:prompt', this.show);
     },
 
@@ -17,6 +23,16 @@
 
     hide: function() {
       this.$el.modal('hide');
+    },
+
+    onSubmit: function(e) {
+      e.preventDefault();
+      console.log(this.getFormData());
+      return false;
+    },
+
+    getFormData: function() {
+      return this.$form.serializeArray();
     }
 
   });
