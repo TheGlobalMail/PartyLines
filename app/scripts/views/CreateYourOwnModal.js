5ce6434fd87f8fdc53e1e1bc7fc8cb1811fbfd14
Andrew Cobby <andrew.cobby@gmail.com>
2013-02-15 12:55:50 +1100

    conflict resoluation fail

--- app/scripts/views/CreateYourOwnModal.js
+++ app/scripts/views/CreateYourOwnModal.js
@@ -7,10 +7,6 @@
 
   var CreateYourOwnModal = Backbone.View.extend({
 
-<<<<<<< HEAD
-    initialize: function() {
-      this.listenTo(app.vent, 'search:prompt', this.show);
-=======
     events: {
       'submit form': 'onSubmit',
       'shown': 'onShown'
@@ -21,7 +17,6 @@
       this.$form = this.$('form');
       this.listenTo(app.vent, 'search:prompt', this.show, this);
       this.listenTo(app.vent, 'search:loaded', this.updateTerms, this);
->>>>>>> custom-searching
     },
 
     show: function() {
@@ -30,8 +25,6 @@
 
     hide: function() {
       this.$el.modal('hide');
-<<<<<<< HEAD
-=======
     },
 
     onSubmit: function(e) {
@@ -65,7 +58,6 @@
       _.each(terms, function(term, i) {
         $inputs.eq(i).val(term);
       });
->>>>>>> custom-searching
     }
 
   });
