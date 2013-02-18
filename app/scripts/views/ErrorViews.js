2019a9a51862fa445a9ca22b54dbd92dcd549a48
Jamie Ferguson <jferguson90210@gmail.com>
2013-02-18 15:11:24 +1100

    Error wording

--- app/scripts/views/ErrorViews.js
+++ app/scripts/views/ErrorViews.js
@@ -6,7 +6,7 @@
     this.message = message;
     this.$el = $('<div/>').addClass('error-container');
     this.$el.css('top', top + 'px');
-    this.template = _.template('<p class="error">The phrase <strong><%= term %></strong> <%= message %>. Please try again and let us know if this message doesn\'t make sense.</p>')
+    this.template = _.template('<p class="error">The phrase <strong><%= term %></strong> <%= message %>. Please try a different search term.</p>')
   };
 
   ErrorMessageView.prototype.render = function() {
@@ -17,7 +17,7 @@
     this.term = term;
     this.$el = $('<div/>').addClass('error-container');
     this.$el.css('top', top + 'px');
-    this.template = _.template('<p class="error">No mentions of <strong><%= term %></strong> found. <br />Try a different term or search <a href"http://www.openaustralia.org/" target="_blank">openaustralia.org</a> for inspiration.</p>')
+    this.template = _.template('<p class="error">No mentions of <strong><%= term %></strong> found. <br />Please try a different search term.</p>')
   };
 
   TermNotFoundView.prototype.render = function() {
